3599d86ef3c4b981e079d103019baddb49c3f43d
Kevin Griffin <me@kevin.jp>
2023-12-23 23:24:28 +0900

    Port to new configuration style

--- nixpkgs/.config/nixpkgs/modules/dns.nix
+++ nixpkgs/.config/nixpkgs/modules/dns.nix
@@ -1,53 +1,63 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 let
   inherit (import ../router-constants.nix) localv4Address v6Address;
-  hosts = import ../hosts.nix;
 in
 {
   services.dnsmasq = {
     enable = true;
 
-    resolveLocalQueries = true;
+    settings = {
+      server = [
+        "8.8.8.8"
+        "8.8.4.4"
+        "2001:4860:4860::8888"
+        "2001:4860:4860::8844"
+      ];
 
-    servers = [
-      "8.8.8.8"
-      "8.8.4.4"
-      "2001:4860:4860::8888"
-      "2001:4860:4860::8844"
-    ];
+      bind-interfaces = true;
 
-    extraConfig = ''
-      no-resolv
+      listen-address = [
+        v6Address
+        localv4Address
+        "127.0.0.1"
+        "::1"
+      ];
 
-      bind-interfaces
-      listen-address=${v6Address},${localv4Address},127.0.0.1,::1
-      conf-file=${pkgs.dnsmasq}/share/dnsmasq/trust-anchors.conf
-      dnssec
+      conf-file = "${pkgs.dnsmasq}/share/dnsmasq/trust-anchors.conf";
 
-      domain=kevin.jp
-      dhcp-range=192.168.11.50,192.168.11.200,255.255.255.0,192.168.11.255,12h
-      dhcp-range=::,static
+      dnssec = true;
 
-      dhcp-host=74:4D:28:FA:46:85,192.168.11.2  # switch
-      dhcp-host=F0:2F:74:FA:14:63,192.168.11.80 # umaru
-      dhcp-host=64:4B:F0:19:F4:9D,192.168.11.92 # tsukihi
-      dhcp-host=90:48:9A:85:70:1A,192.168.11.94 # tvb928217d081b
-      dhcp-host=00:0E:C6:E5:58:F4,192.168.11.98
-      dhcp-host=00:22:CF:F6:C4:C9,192.168.11.96
-      dhcp-host=38:56:10:00:88:89,192.168.11.70 # sesame wifi
+      domain = "kevin.jp";
 
+      dhcp-authoritative = true;
 
-      dhcp-option=option:router,${localv4Address}
-      dhcp-option=option:ntp-server,${localv4Address}
-      dhcp-option=option6:ntp-server,${v6Address}
-      dhcp-option=option6:dns-server,${v6Address}
-      dhcp-option=option6:domain-search,kevin.jp
+      dhcp-range = [
+        "192.168.11.50,192.168.11.200,255.255.255.0,192.168.11.255,12h"
+        "::,static"
+      ];
 
-      dhcp-authoritative
+      dhcp-host = [
+        "74:4D:28:FA:46:85,192.168.11.2" # switch
+        "F0:2F:74:FA:14:63,192.168.11.80" # umaru
+        "64:4B:F0:19:F4:9D,192.168.11.92" # tsukihi
+        "90:48:9A:85:70:1A,192.168.11.94" # tvb928217d081b
+        "00:0E:C6:E5:58:F4,192.168.11.98"
+        "00:22:CF:F6:C4:C9,192.168.11.96"
+        "38:56:10:00:88:89,192.168.11.70" # sesame wifi       
+      ];
 
-      cache-size=10000
-      quiet-dhcp
-    '';
+      dhcp-option = [
+        "option:router,${localv4Address}"
+        "option:ntp-server,${localv4Address}"
+        "option6:ntp-server,${v6Address}"
+        "option6:dns-server,${v6Address}"
+        "option6:domain-search,kevin.jp"
+      ];
+
+      no-resolv = true;
+      cache-size = 10000;
+      quiet-dhcp = true;
+    };
   };
 
   # dnsmasq is configured to bind to the slaac'd address, which
